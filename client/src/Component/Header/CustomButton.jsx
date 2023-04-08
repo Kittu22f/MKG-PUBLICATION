64f70bc39be2829171521154fa21938ec1277865
Divya <divyaagoyal222@gmail.com>
2023-04-08 16:54:24 +0530

    change

--- client/src/Component/Header/CustomButton.jsx
+++ client/src/Component/Header/CustomButton.jsx
@@ -1,5 +1,7 @@
 import { Box,Button, Typography,styled } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LoginDialog from "../login/LoginDailog";
+import { useState } from "react";
 const Wrapper = styled(Box)`
 display:flex;
 margin:0 3% 0 auto;
@@ -23,19 +25,22 @@ const Container = styled(Box)`
 display:flex;
 `
 
-
-
 const CustomButton=()=>{
+    const[open,setOpen]=useState(false);
+    const openDialog=()=>{
+        setOpen(true);
+    }
 return (
 <>
 <Wrapper>
-<LoginButton variant="contained">Login</LoginButton>
+<LoginButton variant="contained" onClick={()=>openDialog()} >Login</LoginButton>
 <Typography style={{marginTop: 3, width:135}}>Become a Seller</Typography>
 <Typography style={{marginTop:3}}>More</Typography>
 <Container>
 <ShoppingCartIcon/>
 <Typography>Cart</Typography>
 </Container>
+<LoginDialog open={open} setOpen={setOpen}/>
 </Wrapper>
 </>  
 )
---
+++ client/src/Component/login/LoginDailog.js
@@ -0,0 +1,72 @@
+import { Box,TextField ,Typography,Button,styled} from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+const Component = styled(Box)`
+ height:70vh;
+ width:90vh;
+display:flex;
+
+
+`;
+const Image = styled(Box)`
+background:#2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 80% no-repeat;
+height:100%;
+width:40%;
+padding : 45px 35px;
+`
+const Wrapper=styled(Box)`
+display:flex;
+flex-direction:column;
+padding:25px 35px;
+flex:1;
+&>div,&>button,&>p(
+    margin-top:20px;
+    )
+`
+const LoginButton=styled(Button)`
+text-transform:none;
+background:#FB641B;
+color:#fff;
+height:48px;
+border-radius:2px;
+
+`;
+const RequestOtp=styled(Button)`
+text-transform:none;
+background:#fff;
+color:#2874f0;
+height:48px;
+border-radius:2px;
+box-shadow:0 2px 4px 0 rgb(0 0 0/ 20%);
+`;
+const Text = styled(Typography)`
+    font-size:12px;
+color:#878787;
+`
+const LoginDialog=({open,setOpen})=>{
+    const handleClose=()=>{
+        setOpen(false);
+    }
+    return (
+        <>
+        <Dialog open={open} onClose={handleClose}>
+        <Component>
+        <Image>
+            <Typography varient ="h5" >Login</Typography>
+            <Typography style={{marginTop:20}}>Get access to your WishList</Typography>
+        </Image>
+        <Wrapper>
+        <TextField variant="standard" label="enter email /Mobile Number"/>
+        <TextField variant="standard" label="enter Password"/>
+       <Typography  style={{marginTop:20}}>By Continue, you agree to Flipkart's term of use and private policy </Typography>
+        <LoginButton  style={{marginTop:20}}>Login</LoginButton>
+        <Typography  style={{marginTop:20}}>OR</Typography>
+        <RequestOtp  style={{marginTop:20}}>Request OTP</RequestOtp>
+        <Typography  style={{marginTop:20}}>New to flipkart? Create an account</Typography>
+        </Wrapper>
+        </Component>
+        </Dialog>
+        </>
+    )
+
+}
+export default LoginDialog;
